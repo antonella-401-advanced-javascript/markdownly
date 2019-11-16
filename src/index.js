1bc7d5c9c0b15e98f82f1f8795d5572042037f46
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-11-15 17:39:59 -0800

    adding some css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import App from './components/App';
-import 'normalize.css';
+import './reset.css';
 import store from './store';
 
 render(
